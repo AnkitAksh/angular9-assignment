084f4a99de48d285946c4c29e8e5f08771cdc833
ankit <ankit.chauhan@classicinformatics.com>
2020-02-19 20:58:55 +0530

    getting data from json instead api

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import * as data from '../../assets/products.json';
+// import * as data from '../../assets/products.json';
 import { Product } from './product.model.js';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -12,7 +12,7 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
   productList: Product[] = [];
-  private GET_PRODUCTS = 'https://jsonplaceholder.typicode.com/photos';
+  private GET_PRODUCTS = 'assets/products.json';
 
   handleError(error: HttpErrorResponse) {
     let errorMessage = 'Unknown error!';
